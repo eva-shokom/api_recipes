c0bb516bf80a90bd69818da98475bf649b5f82c5
Евгения Кузьмина <eva.shokom@yandex.ru>
2024-05-21 13:47:27 +0300

    change stat

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles/static'
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
