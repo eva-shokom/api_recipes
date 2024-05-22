a02f5b28201214c3202a5340e8a61a77763941f5
Евгения Кузьмина <eva.shokom@yandex.ru>
2024-05-22 10:59:22 +0300

    load ingredients

--- backend/recipes/management/commands/import_data.py
+++ backend/recipes/management/commands/import_data.py
@@ -13,7 +13,7 @@ class Command(BaseCommand):
 
     def import_ingredients(self):
         # Ingredient.objects.all().delete()
-        file_path = '../data/ingredients.csv'
+        file_path = 'data/ingredients.csv'
 
         with open(file_path, 'r', encoding='utf-8') as csv_file:
             reader = csv.reader(csv_file)
